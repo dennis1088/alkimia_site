73752b26331240b10c78b9c4183139746c7fbaa8
Lem Lebron <lbl5758@gmail.com>
2012-06-25 21:32:20 -0700

    Removed cache files that were comitted
    Removed files in cache folders which was accidently commited. Also
    removed breadcrumbs addon which was unused.

--- concrete5/packages/swp_breadcrumbs/blocks/swp_breadcrumbs/edit.php
+++
@@ -1,5 +0,0 @@
-<?php   
-defined('C5_EXECUTE') or die(_("Access Denied."));
-?>
-<b><?php  echo t("Breadcrumbs delimiter:"); ?></b><br />
-<input type="text" name="delimiter" size="10" value="<?php  echo htmlspecialchars($delimiter); ?>" /><br />
