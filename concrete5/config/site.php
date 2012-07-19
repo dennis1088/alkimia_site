2102489185907d3f484b4767792083f443710e46
Lem Lebron <lbl5758@gmail.com>
2012-07-18 22:19:36 -0700

    Removed concrete5
    Removed concrete5 from repo in order to install fresh installed version
    that runs faster and contains newest theme.

--- concrete5/config/site.php
+++
@@ -1,8 +0,0 @@
-<?php 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'concrete_db');
-define('PASSWORD_SALT', 'OX3RRA6BSXXqlgmTNz6EK4Zn0S2FEI1Kpuhg9R1w4AkvCK2nq4AYHDbzIJOOV9PU');
-define('ENABLE_NEWSFLOW_OVERLAY', false);
-define('PERMISSIONS_MODEL', 'advanced');
--- concrete5/helpers/miser_support/miser_bridge_JOOMLA.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Cache directories
-$this->css_dir( JPATH_ROOT.'/cache/css/');
-$this->js_dir ( JPATH_ROOT.'/cache/js/');	
-
-// Organisational lists
-$this->keys_no_file(array('print.css'));	
-?>
